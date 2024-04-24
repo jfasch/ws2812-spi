4d9011c4ea4817fd5b1c743aa5661000c309ca8b
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-04-24 18:16:47 +0200

    ultimate bitstream shape: implemented

--- bin/bitset.cpp
+++ bin/bitset.cpp
@@ -8,5 +8,10 @@ int main()
 {
     bitset<100000> big = 0b010;
     cout << sizeof(big) << endl;
+
+    bitset<5> spibit = 0b11000;
+    for (int i=0; i<5; i++)
+        cout << spibit[i] << endl;
+
     return 0;
 }
--- ws2812/ledstrip.cpp
+++ ws2812/ledstrip.cpp
@@ -1,5 +1,8 @@
 #include "ledstrip.h"
 
+#include <iostream>
+using namespace std;
+
 
 void LEDStrip::create_spi_bits(std::vector<SPIBit>& spi_bits) const
 {
@@ -17,10 +20,28 @@ void LEDStrip::create_spi_bits(std::vector<SPIBit>& spi_bits) const
 
 std::vector<uint8_t> spi_bits_to_bitstream(const std::vector<SPIBit>& spi_bits)
 {
-    size_t nbytes = spi_bits.size() * 5 / 8;
-    nbytes += 1; // for termination/reset
+    // size_t nbytes = spi_bits.size() * 5 / 8;
 
-    // hier gschieht ein wunder
+    // nbytes += 1; // for termination/reset
 
-    return std::vector<uint8_t>(nbytes);
+    // return std::vector<uint8_t>(nbytes);
+
+    std::vector<uint8_t> bitstream;
+
+    uint8_t byte = 0;
+    int byte_pos = 7;
+    for (auto spi_bit: spi_bits) {
+        for (int i=4; i>=0; i--) {
+            if (spi_bit[i])
+                byte |= (1<<byte_pos);
+            byte_pos--;
+            if (byte_pos == -1) {
+                bitstream.push_back(byte);
+                byte = 0;
+                byte_pos = 7;
+            }
+        }
+    }
+
+    return bitstream;
 }
